393d6b4cea40486c6e9036aae0043b0dde50a328
mbaradji221 <mbaradji221@outlook.fr>
2019-02-04 16:26:35 +0100

    unlink

--- buttons/linkWithDashBoard.js
+++ buttons/linkWithDashBoard.js
@@ -6,10 +6,14 @@ const {
 } = require("spinal-env-viewer-panel-manager-service");
 
 import ContextGeographicService from "spinal-env-viewer-context-geographic-service";
+import {
+  dashboardService
+} from "spinal-env-viewer-dashboard-standard-service";
 
 
 const CONTEXT_TYPE = ContextGeographicService.constants.CONTEXT_TYPE;
 
+let hasDash = false;
 
 class LinkWithDashBoard extends SpinalContextApp {
   constructor() {
@@ -19,22 +23,46 @@ class LinkWithDashBoard extends SpinalContextApp {
       backgroundColor: "#FF0000",
       fontColor: "#FFFFFF"
     });
+
   }
 
   isShown(option) {
     if (option.context.type.get() === CONTEXT_TYPE && option.selectedNode
       .type
-      .get() !== CONTEXT_TYPE)
-      return Promise.resolve(true);
-    return Promise.resolve(-1);
+      .get() !== CONTEXT_TYPE) {
+      return dashboardService.hasDashBoard(option.selectedNode.id.get()).then(
+        el => {
+          console.log(hasDash, el);
+          hasDash = el;
+          if (el) {
+            this.label = "unlink to dashboard";
+            this.buttonCfg.icon = "link_off";
+          } else {
+            this.label = "Link to a Dashboard";
+            this.buttonCfg.icon = "link";
+          }
+
+          return el;
+        })
+    } else {
+      return Promise.resolve(-1);
+    }
+
+
   }
 
   action(option) {
-    let dialogParams = {
-      context: option.context,
-      selectedNode: option.selectedNode
+    if (hasDash) {
+      dashboardService.unLinkToDashBoard(option.selectedNode.id.get());
+    } else {
+      let dialogParams = {
+        context: option.context,
+        selectedNode: option.selectedNode
+      }
+      spinalPanelManagerService.openPanel('linkToDashBoardDialog',
+        dialogParams);
     }
-    spinalPanelManagerService.openPanel('linkToDashBoardDialog', dialogParams)
+
   }
 }
 
